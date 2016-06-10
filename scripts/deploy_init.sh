9d15a4715f9b4f7f70c1a8b7e39fa995eafccb20
Gor Martsen <Gor.martsen@gmail.com>
2016-06-09 20:14:10 -0400

    Initial commit

---
+++ scripts/deploy_init.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+# Install drush and Console_Table
+
+#prepare DOCROOT
+mkdir $DOCROOT
+
+#install drupal
+sh $ZENCI_DEPLOY_DIR/scripts/drupal_install.sh
+
+echo "Full site path: $DOCROOT"
+
+# Go to domain directory.
+cd $DOCROOT
+
+
+echo "Linking modules from $ZENCI_DEPLOY_DIR"
+
+mkdir -p $DOCROOT/sites/all/modules/contrib
+mkdir -p $DOCROOT/sites/all/themes/contrib
+mkdir -p $DOCROOT/sites/all/libraries
+
+cd $DOCROOT/sites/all/modules
+ln -s $ZENCI_DEPLOY_DIR/modules ./custom
+
+cd $DOCROOT/sites/all/themes
+ln -s $ZENCI_DEPLOY_DIR/themes ./custom
+
+cd $DOCROOT/sites/all/libraries
+ln -s $ZENCI_DEPLOY_DIR/libraries ./custom
+
+
+echo "Enable Modules & themes"
+cd $DOCROOT
+
+for project in `cat $ZENCI_DEPLOY_DIR/settings/enable.list`; do
+  drush -y en $project
+done
+
+if [ "$DEFAULT_THEME" != "" ]; then
+  echo "Set default theme"
+  drush -y en $DEFAULT_THEME
+  drush vset theme_default $DEFAULT_THEME
+fi
+
+if [ "$ENABLE_DEVEL" != "" ]; then
+  drush dl devel
+  drush -y en devel devel_generate
+  drush generate-content 100
+  drush generate-users 100
+fi
---
+++ scripts/drupal_tests.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+php scripts/run-tests.sh --url http://localhost --verbose --all --concurrency 10 --color --verbose 
